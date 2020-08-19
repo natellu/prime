145dea8cb22dc36ece038ccd28b5d36272e5a1d1
natellu <t.le0nh4rdt@gmail.com>
2020-08-19 12:08:30 +0200

    simple prime check

---
+++ Prime/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Prime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {        
+            for(int x = 1; x<= 100; x++)
+            {
+                bool isPrime = true;
+                for (int y = 2; y < (x / 2) + 1; y++)
+                {
+                    if(x%y == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+
+                if (isPrime)
+                {
+                    Console.WriteLine(x);
+                }
+            }
+        }
+    }
+}
